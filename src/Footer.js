6e9b576952a6ddcfaba0fcc66dcd41597f537e22
njambi-cloud <christinemacharia64@gmail.com>
2023-02-11 21:20:52 +0300

    Creates the footer component

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/rajshekhar26/cleanfolio'
+      className='link footer__link'
+    >
+      Created By Eliazar Agwaya
+    </a>
+  </footer>
+)
+
+export default Footer
