8f35c0b185464ab6068277fcaff64b25ee08e1fa
njambi-cloud <christinemacharia64@gmail.com>
2023-02-11 21:33:26 +0300

    Works on the importation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { useContext } from 'react'
+import { ThemeContext } from './theme'
+import Header from './components/Header'
+import About from './components/About'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+import ScrollToTop from './components/ScrollToTop'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import './App.css'
 
-import './App.css';
+const App = () => {
+  const [{ themeName }] = useContext(ThemeContext)
 
-function App() {
   return (
-    <div className="App">
-     
+    <div id='top' className={`${themeName} app`}>
+      <Header />
+
+      <main>
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+      </main>
+
+      <ScrollToTop />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
